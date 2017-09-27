c8f7c33736d6133c9816c9a4c9803a9c6bd927f5
Arion <s3650046@student.rmit.edu.au>
2017-09-27 14:54:53 +1000

    Removed checks for initial values

--- Functions.R
+++ Functions.R
@@ -15,13 +15,12 @@ expSmooth <- function(ts) {
   # array to store models
   models <- list()
   model.info <- array(NA,
-                      dim = c(700,8), 
-                      dimnames = list(NULL,c("ID","MASE","Shapiro-Wilks","Transformed","Lambda","Series Added Value","BC Added Value","BC Division")))
+                      dim = c(700,6), 
+                      dimnames = list(NULL,c("ID","MASE","Shapiro-Wilks","Transformed","Lambda","Added Value")))
   
   # model options
   exponential <- c(TRUE,FALSE)
   damped <- c(TRUE,FALSE)
-  initial <- c("optimal","simple")
   method <- c("ANN","ANA","MNN","MNA","MNM")
   methodDamp <- c("AAN","AAA","MAN","MMN","MAA","MAM","MMM")  
   opt.crit <- c("lik","amse","mse","sigma","mae")
@@ -32,7 +31,6 @@ expSmooth <- function(ts) {
   ts.list[[1]] <- ts
   
   # adjusting negative series & box-cox transformations
-  scale <- NA
   min_value <- min(ts)
   add_value <- 0
   if(min_value <= 0){
@@ -40,19 +38,11 @@ expSmooth <- function(ts) {
     ts_add <- ts + add_value
     lambda <- BoxCox.lambda(ts_add, method = "loglik")
     bc <- BoxCox(ts_add,lambda)
-    if(max(bc)>1e7){
-      scale <- max(bc)/1e6
-      bc <- bc/scale
-    }
     ts.list[[2]] <- bc
     ts.list[[3]] <- ts_add
   }else{
     lambda <- BoxCox.lambda(ts, method = "loglik")
     bc <- BoxCox(ts,lambda)
-    if(bc>1e7){
-      scale <- max(bc)/1e6
-      bc <- bc/scale
-    }
     ts.list[[2]] <- bc
   }
   
@@ -60,167 +50,132 @@ expSmooth <- function(ts) {
   # array row counter
   count <- 1
   
-  for(i in 1:2){
     for(j in 1:2){
-      model <- holt(ts.list[[j]], damped = TRUE, exponential = FALSE, initial = initial[i])
+      model <- holt(ts.list[[j]], damped = TRUE, exponential = FALSE)
       models[[count]] <- model
       model.info[count,"ID"] <- count
       model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
       model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-      model.info[count,"Series Added Value"] <- 0
+      model.info[count,"Added Value"] <- 0
       if(j == 1){
         model.info[count,"Transformed"] <- FALSE
         model.info[count,"Lambda"] <- NA
-        model.info[count,"BC Division"] <- NA
-        model.info[count,"BC Added Value"] <- NA
       }else{
         model.info[count,"Transformed"] <- TRUE
         model.info[count,"Lambda"] <- lambda
-        model.info[count,"BC Division"] <- scale
-        model.info[count,"BC Added Value"] <- add_value
       }
       
       count <- count + 1
       
-      model <- ses(ts.list[[j]], initial = initial[i])
+      model <- ses(ts.list[[j]])
       models[[count]] <- model
       model.info[count,"ID"] <- count
       model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
       model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-      model.info[count,"Series Added Value"] <- 0
+      model.info[count,"Added Value"] <- 0
       if(j == 1){
         model.info[count,"Transformed"] <- FALSE
         model.info[count,"Lambda"] <- NA
-        model.info[count,"BC Division"] <- NA
-        model.info[count,"BC Added Value"] <- NA
       }else{
         model.info[count,"Transformed"] <- TRUE
         model.info[count,"Lambda"] <- lambda
-        model.info[count,"BC Division"] <- scale
-        model.info[count,"BC Added Value"] <- add_value
       }
       
       count <- count + 1
     }
-  }
   
   
-  # option grids
-  expand.ei <- expand.grid(exponential, initial, stringsAsFactors = FALSE)
-  expand.di <- expand.grid(damped, initial, stringsAsFactors = FALSE)
-  
-  for(i in 1:4){
     for(j in 1:2){
-      if(expand.ei[i,1] == TRUE && min_value <= 0){
-        model <- holt(ts.list[[j+1]], damped = FALSE, exponential = expand.ei[i,1], initial = expand.ei[i,2])
+      if(exponential[j] == TRUE && min_value <= 0){
+        model <- holt(ts.list[[j+1]], damped = FALSE, exponential = exponential[j])
         models[[count]] <- model
         model.info[count,"ID"] <- count
         model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
         model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- add_value
+        model.info[count,"Added Value"] <- 0
         if(j == 2){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
       }else{
-        model <- holt(ts.list[[j]], damped = FALSE, exponential = expand.ei[i,1], initial = expand.ei[i,2])
+        model <- holt(ts.list[[j]], damped = FALSE, exponential = exponential[j])
         models[[count]] <- model
         model.info[count,"ID"] <- count
         model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
         model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- 0
+        model.info[count,"Added Value"] <- 0
         if(j == 1){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
       }
       
       
-      model <- hw(ts.list[[j]], damped = expand.di[i,1], exponential = FALSE, initial = expand.di[i,2], 
+      model <- hw(ts.list[[j]], damped = damped[j], exponential = FALSE, 
                   seasonal = "additive")
       models[[count]] <- model
       model.info[count,"ID"] <- count
       model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
       model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-      model.info[count,"Series Added Value"] <- 0
+      model.info[count,"Added Value"] <- 0
       if(j == 1){
         model.info[count,"Transformed"] <- FALSE
         model.info[count,"Lambda"] <- NA
-        model.info[count,"BC Division"] <- NA
-        model.info[count,"BC Added Value"] <- NA
       }else{
         model.info[count,"Transformed"] <- TRUE
         model.info[count,"Lambda"] <- lambda
-        model.info[count,"BC Division"] <- scale
-        model.info[count,"BC Added Value"] <- add_value
       }
       
       count <- count + 1
     }
-  }
+  
   
   # option grid
-  expand.dei <- expand.grid(damped, exponential, initial, stringsAsFactors = FALSE)
+  expand.de <- expand.grid(damped, exponential, stringsAsFactors = FALSE)
   
-  for(i in 1:8){
+  for(i in 1:4){
     for(j in 1:2){
       if(min_value <= 0){
-        model <- hw(ts.list[[j+1]], damped = expand.dei[i,1], exponential = expand.dei[i,2], 
-                    seasonal = "multiplicative", initial = expand.dei[i,3])
+        model <- hw(ts.list[[j+1]], damped = expand.de[i,1], exponential = expand.de[i,2], 
+                    seasonal = "multiplicative")
         models[[count]] <- model
         model.info[count,"ID"] <- count
         model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
         model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- add_value
+        model.info[count,"Added Value"] <- 0
         if(j == 2){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
       }else{
-        model <- hw(ts.list[[j]], damped = expand.dei[i,1], exponential = expand.dei[i,2], 
-                    seasonal = "multiplicative", initial = expand.dei[i,3])
+        model <- hw(ts.list[[j]], damped = expand.de[i,1], exponential = expand.de[i,2], 
+                    seasonal = "multiplicative")
         models[[count]] <- model
         model.info[count,"ID"] <- count
         model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
         model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- 0
+        model.info[count,"Added Value"] <- 0
         if(j == 1){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
@@ -238,19 +193,15 @@ expSmooth <- function(ts) {
                      bounds = expand.mob[i,3])
         models[[count]] <- model
         model.info[count,"ID"] <- count
-        model.info[count,"MASE"] <- accuracy(model)[1,"MASE"]
-        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- add_value
+        model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
+        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
+        model.info[count,"Added Value"] <- 0
         if(j == 2){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
@@ -259,19 +210,15 @@ expSmooth <- function(ts) {
                      bounds = expand.mob[i,3])
         models[[count]] <- model
         model.info[count,"ID"] <- count
-        model.info[count,"MASE"] <- accuracy(model)[1,"MASE"]
-        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- 0
+        model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
+        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
+        model.info[count,"Added Value"] <- 0
         if(j == 1){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
@@ -290,19 +237,15 @@ expSmooth <- function(ts) {
                      bounds = expand.mdob[i,4])
         models[[count]] <- model
         model.info[count,"ID"] <- count
-        model.info[count,"MASE"] <- accuracy(model)[1,"MASE"]
-        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- add_value
+        model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
+        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
+        model.info[count,"Added Value"] <- 0
         if(j == 2){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
@@ -311,19 +254,15 @@ expSmooth <- function(ts) {
                      bounds = expand.mdob[i,4])
         models[[count]] <- model
         model.info[count,"ID"] <- count
-        model.info[count,"MASE"] <- accuracy(model)[1,"MASE"]
-        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$residuals)$p.value
-        model.info[count,"Series Added Value"] <- 0
+        model.info[count,"MASE"] <- accuracy(model$model)[1,"MASE"]
+        model.info[count,"Shapiro-Wilks"] <- shapiro.test(model$model$residuals)$p.value
+        model.info[count,"Added Value"] <- 0
         if(j == 1){
           model.info[count,"Transformed"] <- FALSE
           model.info[count,"Lambda"] <- NA
-          model.info[count,"BC Division"] <- NA
-          model.info[count,"BC Added Value"] <- NA
         }else{
           model.info[count,"Transformed"] <- TRUE
           model.info[count,"Lambda"] <- lambda
-          model.info[count,"BC Division"] <- scale
-          model.info[count,"BC Added Value"] <- add_value
         }
         
         count <- count + 1
